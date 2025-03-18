8db6edd6cdaf7eca00aa95f897a788bb3c5815e3
bonnieblack14 <sasha.val2006@gmail.com>
2025-03-18 15:29:52 +0300

    Simple interaction with minio created (uploading in bucket)

---
+++ app/minio_utils.py
@@ -0,0 +1,27 @@
+import boto3
+import config
+
+s3 = boto3.client(
+    "s3",
+    endpoint_url=config.MINIO_ENDPOINT,
+    aws_access_key_id=config.MINIO_ACCESS_KEY,
+    aws_secret_access_key=config.MINIO_SECRET_KEY
+)
+
+
+def create_bucket():
+    try:
+        s3.head_bucket(Bucket=config.MINIO_BUCKET)
+    except:
+        s3.create_bucket(Bucket=config.MINIO_BUCKET)
+
+
+create_bucket()
+
+
+def upload_file_to_minio(file_path, minio_key):
+    s3.upload_file(file_path, config.MINIO_BUCKET, minio_key)
+    return f"{config.MINIO_ENDPOINT}/{config.MINIO_BUCKET}/{minio_key}"
+
+
+
